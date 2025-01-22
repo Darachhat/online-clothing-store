7b70f2938f1f2270d230bb2937637bb6b9e512df
Darachhat SOTHUN <129612348+Darachhat@users.noreply.github.com>
2025-01-22 15:19:13 +0700

    update vercel json

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,21 +1,43 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import { useSearchParams } from 'react-router-dom';
+import React, { useContext, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Verify = () => {
+  const { navigate, token, setCartItems, backendUrl } = useContext(ShopContext);
+  const { searchParams, setSearchParams } = useSearchParams();
 
-    const { navigate, token, setCartItems } =useContext(ShopContext);
-    const { searchParams, setSearchParams  } = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
 
-    const verifyPayment = async () => {
+  const verifyPayment = async () => {
+    try {
+      if (!token) {
+        return null;
+      }
 
+      const response = await axios.post(
+        backendUrl + "/api/order/verifyAba",
+        { success, orderId },
+        { headers: { token } }
+      );
+
+      if (response.data.success) {
+        setCartItems({})
+        navigate('/orders')
+      } else {
+        navigate("/cart")
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
     }
+  };
 
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  useEffect(() => {}, [token]);
+  verifyPayment();
+  return <div></div>;
+};
 
-export default Verify
+export default Verify;
